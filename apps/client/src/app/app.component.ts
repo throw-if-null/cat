e29843533f481830a087d2319e4da5b983f642de
DrDreo <code@andreas-hahn.at>
2021-07-03 12:23:59 +0200

    setup 2

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,42 +1,40 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Subject } from 'rxjs';
 import { filter, map, takeUntil } from 'rxjs/operators';
 
 @Component({
-	selector: 'rat-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'rat-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
+  title = '';
 
-	title = '';
+  private unsubscribe$ = new Subject();
 
-	private unsubscribe$ = new Subject();
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
-	constructor(private router: Router, private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.router.events
+        .pipe(
+          filter((event) => event instanceof NavigationEnd),
+          map(() => {
+            const child = this.route.firstChild;
+            if (child?.snapshot.data['title']) {
+              return child.snapshot.data['title'];
+            }
+            return 'No Title Data';
+          }),
+          takeUntil(this.unsubscribe$)
+        )
+        .subscribe((title: string) => {
+          this.title = title;
+        });
+  }
 
-	ngOnInit() {
-		this.router.events
-			.pipe(
-				filter(event => event instanceof NavigationEnd),
-				map(() => {
-					const child = this.route.firstChild;
-					console.log(this.route);
-					if (child?.snapshot.data['title']) {
-						return child.snapshot.data['title'];
-					}
-					return 'No Title Data';
-				}),
-				takeUntil(this.unsubscribe$)
-			)
-			.subscribe((title: string) => {
-				this.title = title;
-			});
-	}
-
-	ngOnDestroy(): void {
-		this.unsubscribe$.next();
-		this.unsubscribe$.complete();
-	}
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
