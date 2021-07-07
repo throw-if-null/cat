f8ab2e0989eabb260a96951b037d69707a1186e1
DrDreo <code@andreas-hahn.at>
2021-07-07 22:20:40 +0200

    fix tests and update

--- apps/client/src/app/pages/projects/config-create/config-create.component.spec.ts
+++ apps/client/src/app/pages/projects/config-create/config-create.component.spec.ts
@@ -1,25 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ConfigCreateComponent } from './config-create.component';
 
 describe('ConfigCreateComponent', () => {
-  let component: ConfigCreateComponent;
-  let fixture: ComponentFixture<ConfigCreateComponent>;
+	let component: ConfigCreateComponent;
+	let fixture: ComponentFixture<ConfigCreateComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ConfigCreateComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ConfigCreateComponent],
+			imports: [ReactiveFormsModule],
+		}).compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ConfigCreateComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ConfigCreateComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- apps/client/src/app/pages/projects/project-details/project-details.component.spec.ts
+++ apps/client/src/app/pages/projects/project-details/project-details.component.spec.ts
@@ -1,6 +1,7 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DialogService } from '@ngneat/dialog';
 
 import { ProjectDetailsComponent } from './project-details.component';
 
@@ -12,7 +13,8 @@ describe('ProjectDetailsComponent', () => {
 		await TestBed.configureTestingModule({
 			imports: [RouterTestingModule],
 			declarations: [ProjectDetailsComponent],
-			schemas: [NO_ERRORS_SCHEMA]
+			providers: [{ provide: DialogService, useValue: {} }],
+			schemas: [NO_ERRORS_SCHEMA],
 		}).compileComponents();
 	});
 
