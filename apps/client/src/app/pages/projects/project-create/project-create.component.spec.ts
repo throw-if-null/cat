6c8feb71ac8276d27f603a81a8930472b3b48f48
DrDreo <code@andreas-hahn.at>
2021-09-04 17:36:40 +0200

    updated tests

--- apps/client/src/app/pages/projects/project-create/project-create.component.spec.ts
+++ apps/client/src/app/pages/projects/project-create/project-create.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DialogRef } from '@ngneat/dialog';
+import { ProjectTypeNamePipe } from '../../../project-name.pipe';
 
 import { ProjectCreateComponent } from './project-create.component';
 
@@ -10,7 +11,7 @@ describe('ProjectCreateComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [ProjectCreateComponent],
+			declarations: [ProjectCreateComponent, ProjectTypeNamePipe],
 			providers: [{ provide: DialogRef, useValue: {} }],
 			imports: [ReactiveFormsModule]
 		}).compileComponents();
