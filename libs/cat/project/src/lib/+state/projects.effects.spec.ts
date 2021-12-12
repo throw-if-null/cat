cb7ef926bb967be987fe983fd3c94c92e95a5e03
DrDreo <code@andreas-hahn.at>
2021-12-12 17:37:08 +0100

    migrate angular

--- libs/cat/project/src/lib/+state/projects.effects.spec.ts
+++ libs/cat/project/src/lib/+state/projects.effects.spec.ts
@@ -3,13 +3,13 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { Action } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 import { NxModule } from '@nrwl/angular';
-import { hot } from '@nrwl/angular/testing';
 import { Observable } from 'rxjs';
 
 import * as ProjectsActions from './projects.actions';
 import { ProjectsEffects } from './projects.effects';
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { MockProjectService, ProjectService, testProject1 } from "@cat/project";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MockProjectService, ProjectService, testProject1 } from '@cat/project';
+import { hot } from 'jasmine-marbles';
 
 describe('ProjectsEffects', () => {
 	let actions: Observable<Action>;
@@ -22,8 +22,8 @@ describe('ProjectsEffects', () => {
 				ProjectsEffects,
 				provideMockActions(() => actions),
 				provideMockStore(),
-				{ provide: ProjectService, useClass: MockProjectService }
-			]
+				{ provide: ProjectService, useClass: MockProjectService },
+			],
 		});
 
 		effects = TestBed.inject(ProjectsEffects);
@@ -34,7 +34,7 @@ describe('ProjectsEffects', () => {
 			actions = hot('-a-|', { a: ProjectsActions.init() });
 
 			const expected = hot('-a-|', {
-				a: ProjectsActions.loadProjectsSuccess({ projects: [ testProject1 ] })
+				a: ProjectsActions.loadProjectsSuccess({ projects: [ testProject1 ] }),
 			});
 
 			expect(effects.init$).toBeObservable(expected);
