433f784413100ba08ac17cc7544f7496910be5aa
DrDreo <code@andreas-hahn.at>
2022-03-22 22:53:03 +0100

    add string parser

---
+++ libs/cat/utils/src/lib/parser.spec.ts
@@ -0,0 +1,149 @@
+import { parseConfigEntries } from "./parser";
+
+
+describe('Parser', () => {
+
+	const configuration1 = [
+		{
+			key: 'level1.test',
+			value: 1
+		},
+		{
+			key: 'level1.sick',
+			value: 'hallo'
+		},
+		{
+			key: 'my_bool',
+			value: true
+		}
+	];
+
+	const flatConfiguration = [
+		{
+			key: 'level1',
+			value: 1
+		},
+		{
+			key: 'level2',
+			value: 'hallo'
+		},
+		{
+			key: 'level3',
+			value: true
+		}
+	];
+
+	const deepConfiguration = [
+		{
+			key: 'level1.test',
+			value: 1
+		},
+		{
+			key: 'level1.level2.nope',
+			value: 'level2'
+		},
+		{
+			key: 'level1.level2.yes.asd',
+			value: 'deep'
+		},
+		{
+			key: 'level1.level2.yes.asdf',
+			value: 'deep'
+		}
+	];
+
+	const weirdConfiguration = [
+		{
+			key: '123',
+			value: 'level2'
+		},
+		{
+			key: '!@#$',
+			value: 1
+		}
+	];
+
+	const overwriteConfiguration = [
+		{
+			key: 'level1.test',
+			value: 1
+		},
+		{
+			key: 'level1.test',
+			value: 'level2'
+		}
+	];
+
+	it('should produce an object', () => {
+
+		const parsedConfig = parseConfigEntries(configuration1);
+
+		expect(parsedConfig).toEqual({
+			level1: {
+				test: 1,
+				sick: 'hallo'
+			},
+			my_bool: true
+		});
+	});
+
+	it('should produce a flat object', () => {
+		const parsedConfig = parseConfigEntries(flatConfiguration);
+
+		expect(parsedConfig).toEqual({
+			level1: 1,
+			level2: 'hallo',
+			level3: true,
+		});
+	});
+
+	it('should produce an complex object', () => {
+
+		const parsedConfig = parseConfigEntries(deepConfiguration);
+
+		expect(parsedConfig).toEqual({
+			level1: {
+				test: 1,
+				level2: {
+					nope: 'level2',
+					yes: {
+						asd: 'deep',
+						asdf: 'deep'
+					}
+				}
+			},
+		});
+	});
+
+	it('should create an object with weird keys', () => {
+		expect(parseConfigEntries(weirdConfiguration)).toEqual(
+			{
+				"123": "level2",
+				"!@#$": 1
+			});
+	});
+
+	it.skip('should throw when overwriting existing value', () => {
+		expect(() => parseConfigEntries(overwriteConfiguration)).toThrow();
+	});
+
+	describe('types', () => {
+		it('should produce a value of type {number}', () => {
+			const parsedConfig: any = parseConfigEntries(configuration1);
+
+			expect(typeof parsedConfig.level1.test).toEqual('number');
+		});
+
+		it('should produce a value of type {string}', () => {
+			const parsedConfig: any = parseConfigEntries(configuration1);
+
+			expect(typeof parsedConfig.level1.sick).toEqual('string');
+		});
+
+		it('should produce a value of type {boolean}', () => {
+			const parsedConfig: any = parseConfigEntries(configuration1);
+
+			expect(typeof parsedConfig.my_bool).toEqual('boolean');
+		});
+	});
+});
