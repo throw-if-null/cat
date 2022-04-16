321e62a114f356147ad324df3cedbf37523a150e
DrDreo <code@andreas-hahn.at>
2022-04-16 19:34:50 +0200

    run only affected tests

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,6 @@ const { getJestProjects } = require('@nrwl/jest');
 
 module.exports = {
 	projects: [
-		...getJestProjects(),
-		'<rootDir>/apps/client',
-		'<rootDir>/libs/ui',
+		...getJestProjects()
 	]
 };
--- jest.preset.js
+++ jest.preset.js
@@ -1,6 +1,5 @@
 const nxPreset = require('@nrwl/jest/preset');
 
 module.exports = {
-    ...nxPreset,
-    coverageReporters: ['lcov', 'text-summary'],
+    ...nxPreset
 };
